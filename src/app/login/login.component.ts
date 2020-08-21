cb7b36685961c36ad1f31fd7bd067b791d1fb069
krupavarma <krupavarma1995@gmail.com>
2020-08-21 14:45:20 +0530

    login page design and validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from "@angular/forms";
 import { Login } from '../shared/entities/login';
 import { AppService } from './../shared/service/app.service';
 import { APIService } from '../service';
-
+import {FormControl, Validators,FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,12 +11,36 @@ import { APIService } from '../service';
 })
 export class LoginComponent implements OnInit {
   loginObj: any = new Login();
- 
+  hide = true;
+  userName = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.email]);
+  myForm: FormGroup;
   constructor(public appService: AppService, private apiService: APIService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    console.log(this.loginObj);
+    this.myForm = new FormGroup({
+      'userName': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    })
+   }
 
-  onSubmit(loginForm: any) {
+  onSubmit() {
+    console.log(this.loginObj)
    
   }
+  getErrorMessage(type) {
+    if(type == 'userName'){
+      if (this.userName.hasError('required')) {
+        return 'User Name Required';
+      }
+    }
+    else{
+      if (this.userName.hasError('required')) {
+        return 'Password Required';
+      }
+    }
+    
+  }
+ 
 }
